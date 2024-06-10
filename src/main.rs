d7f5d617e42ea34dc0e9d79ea945dbbfa3e0f9b1
noreplydev <contact.cristiansanchez@gmail.com>
2024-06-10 20:39:08 +0200

    Add basics models for the vm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+struct Instruction {
+    opcode: OpCode,
+}
+
+impl Instruction {
+    fn new(opcode: OpCode) -> Instruction {
+        Instruction { opcode }
+    }
+}
+
+enum OpCode {
+    LoadConst,
+}
+
+struct Vm {
+    instructions: Vec<Instruction>,
+}
+
+impl Vm {
+    fn new(instructions: Vec<Instruction>) -> Vm {
+        Vm { instructions }
+    }
+    fn run(&self) {
+        for instruction in &self.instructions {
+            match instruction.opcode {
+                OpCode::LoadConst => {
+                    println!("LoadConst");
+                }
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let instructions = vec![Instruction::new(OpCode::LoadConst)];
+    let vm = Vm::new(instructions);
+
+    vm.run()
 }

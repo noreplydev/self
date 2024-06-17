c3c3092feab4e9267a3ec2a3730bf2b8934fd0d6
noreplydev <contact.cristiansanchez@gmail.com>
2024-06-17 02:05:44 +0200

    Update number to bytes functions signature

--- examples/run_vm.rs
+++ examples/run_vm.rs
@@ -1,11 +1,14 @@
-use self_vm::{utils::u64_to_bytes, vm::Vm};
+use self_vm::{
+    utils::{bytes_from_32, bytes_from_64, Number},
+    vm::Vm,
+};
 
 fn main() {
     let mut instructions: Vec<u8> = vec![0x01, 0x01];
-    instructions.extend_from_slice(&u64_to_bytes(14));
+    instructions.extend_from_slice(&bytes_from_64(Number::I64(14)));
     instructions.push(0x01);
-    instructions.push(0x01);
-    instructions.extend_from_slice(&u64_to_bytes(20));
+    instructions.push(0x02);
+    instructions.extend_from_slice(&bytes_from_32(Number::U32(20)));
     instructions.push(0x02);
 
     let mut vm = Vm::new(instructions);
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,28 @@
-pub fn u64_to_bytes(num: u64) -> [u8; 8] {
-    num.to_le_bytes()
+pub enum Number {
+    U64(u64),
+    U32(u32),
+    I64(i64),
+    I32(i32),
 }
 
-pub fn i64_to_bytes(num: i64) -> [u8; 8] {
-    num.to_le_bytes()
+pub fn bytes_from_64(num: Number) -> [u8; 8] {
+    match num {
+        Number::U64(v) => v.to_le_bytes(),
+        Number::I64(v) => v.to_le_bytes(),
+        _ => {
+            println!("Bad type to get bytes from");
+            std::process::exit(1);
+        }
+    }
+}
+
+pub fn bytes_from_32(num: Number) -> [u8; 4] {
+    match num {
+        Number::U32(v) => v.to_le_bytes(),
+        Number::I32(v) => v.to_le_bytes(),
+        _ => {
+            println!("Bad type to get bytes from");
+            std::process::exit(1);
+        }
+    }
 }

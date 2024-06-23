1aca94a8bdf99221baff63513f109f06c05d80db
noreplydev <contact.cristiansanchez@gmail.com>
2024-06-23 21:45:51 +0200

    Update self implementation example

--- examples/run_vm.rs
+++ examples/run_vm.rs
@@ -4,12 +4,14 @@ use self_vm::{
 };
 
 fn main() {
+    // print two nums
     let mut instructions: Vec<u8> = vec![0x01, 0x01];
-    instructions.extend_from_slice(&bytes_from_64(Number::I64(14)));
+    instructions.extend_from_slice(&bytes_from_32(Number::U32(14)));
     instructions.push(0x01);
     instructions.push(0x02);
-    instructions.extend_from_slice(&bytes_from_32(Number::U32(20)));
+    instructions.extend_from_slice(&bytes_from_64(Number::U64(20)));
     instructions.push(0x02);
+    instructions.extend_from_slice(&bytes_from_32(Number::U32(2)));
 
     let mut vm = Vm::new(instructions);
     vm.run();

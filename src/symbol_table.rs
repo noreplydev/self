ee6e03e46a00d50b380139c0c6fcef66989d50fe
noreplydev <contact.cristiansanchez@gmail.com>
2024-07-07 20:45:31 +0200

    key/value addition supported now on the SystemTable

--- src/symbol_table.rs
+++ src/symbol_table.rs
@@ -3,12 +3,18 @@ use std::collections::HashMap;
 
 pub struct SymbolTable {
     scopes: Vec<HashMap<String, Value>>,
+    sc: usize,
 }
 
 impl SymbolTable {
     pub fn new() -> SymbolTable {
         SymbolTable {
             scopes: vec![HashMap::new()],
+            sc: 0,
         }
+    }
+
+    pub fn add_key_value(&mut self, key: String, value: Value) -> Option<Value> {
+        self.scopes[self.sc].insert(key, value)
     }
 }
